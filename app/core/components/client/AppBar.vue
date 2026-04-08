2615b5c83f4e9bcfbcd43741c4f454f19aeb90ef
luxeshadow <agodaclarisse@gmail.com>
2026-04-08 12:24:26 +0100

    feat: fliyer-modal-3

--- app/core/components/client/AppBar.vue
+++ app/core/components/client/AppBar.vue
@@ -159,9 +159,9 @@ const closeModal = () => {
   background-color: white;
   border-radius: 24px;
   box-shadow: 0 20px 40px rgba(0, 0, 0, 0.3);
-  overflow: visible; /* Permet au bouton de dépasser légèrement si besoin */
+  overflow: visible; 
   padding: 10px;
-  margin: 10px;
+  margin: 20px;
 }
 
 /* Style du bouton X "Liquide Glacé" */
