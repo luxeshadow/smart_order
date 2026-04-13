e4d86b548084569c222e7ccf48a2fcb0cbee0e0a
luxeshadow <agodaclarisse@gmail.com>
2026-04-13 13:11:25 +0100

    feat: dashboard-26

--- app/core/constants/app_icons.ts
+++ app/core/constants/app_icons.ts
@@ -1,7 +1,7 @@
 export class AppIcon {
   static readonly add = 'fi fi-rr-plus'
   static readonly cross = 'fi fi-rr-cross'
-  static readonly order = 'fi fi-rr-time-past'    
+  static readonly order = 'fi fi-rr-document-signed'    
   static readonly box = 'fi fi-rr-shopping-cart'     
   static readonly user = 'fi fi-rr-user-gear'   
 }
