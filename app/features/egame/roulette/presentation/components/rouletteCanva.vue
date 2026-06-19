b4b073dd585d26f930106a2d02f95b433e5c45c8
luxeshadow <agodaclarisse@gmail.com>
2026-06-20 00:56:13 +0100

    feat: games features 34

--- app/features/egame/roulette/presentation/components/rouletteCanva.vue
+++ app/features/egame/roulette/presentation/components/rouletteCanva.vue
@@ -140,6 +140,75 @@ const spinWheel = async () => {
 
 onMounted(fetchBalance)
 </script>
+
+<template>
+  <div id="roulette-root">
+    <nav class="app-bar">
+      <button class="back-btn" @click="router.back()">
+        <i class="fi fi-rr-arrow-small-left"></i>
+      </button>
+      <span class="app-bar-title">E-games</span>
+      <div class="spacer"></div>
+    </nav>
+
+    <div class="top-bar">
+      <span class="title-label">Lucky Wheel</span>
+      <span class="balance-badge">
+        Solde Principal : <span class="amount">{{ formatBalance(mainBalance) }}</span> XOF
+      </span>
+    </div>
+
+    <div class="bet-container">
+      <label for="bet-input">Mise (Min 500) :</label>
+      <input 
+        type="number" 
+        id="bet-input" 
+        v-model.number="betInput" 
+        min="500" 
+        :disabled="isSpinning"
+      >
+    </div>
+
+    <section class="wrapper" data-items="12">
+      <div class="controls" :class="{ ticking: isSpinning }">
+        <button id="spin-btn" @click="spinWheel" :disabled="isSpinning">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M14 12a2 2 0 1 0 -4 0a2 2 0 0 0 4 0" />
+            <path d="M12 21c-3.314 0 -6 -2.462 -6 -5.5s2.686 -5.5 6 -5.5" />
+            <path d="M21 12c0 3.314 -2.462 6 -5.5 6s-5.5 -2.686 -5.5 -6" />
+            <path d="M12 14c3.314 0 6 -2.462 6 -5.5s-2.686 -5.5 -6 -5.5" />
+            <path d="M14 12c0 -3.314 -2.462 -6 -5.5 -6s-5.5 2.686 -5.5 6" />
+          </svg>
+        </button>
+      </div>
+      
+      <div 
+        id="wheel" 
+        class="wheel" 
+        :style="{ transform: `rotate(${currentRotation}deg)` }"
+      >
+        <span 
+          v-for="(slice, index) in slices" 
+          :key="index"
+          :class="['slice-item', slice.type]"
+          :style="{ '--offset-dist': `${((index + 1) / 12) * 100}%` }"
+        >
+          {{ slice.label }}
+        </span>
+      </div>
+    </section>
+
+    <div id="message" class="message" :style="{ color: msgColor }">
+      {{ msgText }}
+    </div>
+
+    <div class="debug-info">
+      Winning Index: {{ debugInfo.winning }} | Detected: {{ debugInfo.detected }}
+    </div>
+  </div>
+</template>
+
 <style scoped>
 @import url('https://fonts.bunny.net/css?family=jura:300,700');
 
