e31a5a3a9f711657d7cb86d524fb056aacd57835
luxeshadow <agodaclarisse@gmail.com>
2026-06-19 23:08:56 +0100

    feat: games features 32

--- app/pages/game/e-game.vue
+++ app/pages/game/e-game.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import RouletteCanva from '~/features/egame/roulette/presentation/components/rouletteCanva.vue';
 import planeCanva from '../../features/egame/plane/presentation/components/planeCanva.vue'
+import Footer from '@/core/components/client/mobile/Footer.vue'
 </script>
 
 <template>
   <RouletteCanva />
+     <Footer />
 </template>
