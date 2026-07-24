61a2f49ae4e2eb33f1abade81bd0257a6263702f
luxeshadow <agodaclarisse@gmail.com>
2026-07-24 03:38:31 +0100

    feat: games features pachinko-ultimate_14

--- app/features/transaction/presentation/validators/deposit_validator.ts
+++ app/features/transaction/presentation/validators/deposit_validator.ts
@@ -15,8 +15,8 @@ export class DepositValidator {
       return "Le montant doit être un nombre supérieur à 0.";
     }
 
-    if (amountValue < 1) {
-      return "Le montant minimum est de 1 FCFA.";
+    if (amountValue < 1000) {
+      return "Le montant minimum est de 1000 FCFA.";
     }
 
     return null;
