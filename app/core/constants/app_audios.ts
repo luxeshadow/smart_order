86d4ae89bc2e73889ab714522e897b8ec915d967
luxeshadow <agodaclarisse@gmail.com>
2026-07-24 03:11:57 +0100

    feat: games features pachinko-ultimate_13

--- app/core/constants/app_audios.ts
+++ app/core/constants/app_audios.ts
@@ -2,6 +2,6 @@ export class AppAudio {
 
     static readonly Win_Ringtone = '/assets/audios/ringtone/win.mpeg'
     static readonly Mario_Ringtone = '/assets/audios/ringtone/mario.mpeg'
-     static readonly bomb_Ringtone = '/assets/audios/ringtone/boomb.mpeg'
+     static readonly bomb_Ringtone = '/assets/audios/ringtone/boom.mpeg'
   
 }
